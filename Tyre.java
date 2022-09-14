5665c62880ef4e7f49357846361fd51aa87080c3
Vijayalakshmi T.G <vijayalakshmitg.xworkz@gmail.com>
2022-09-14 10:39:56 +0530

    test

---
+++ Tyre.java
@@ -0,0 +1,28 @@
+class Tyre
+{
+	static void smooth()
+	{
+		System.out.println("gives smooth ride");
+	}
+	static void support()
+	{
+		System.out.println("Supports vehicles weight");
+	}
+	static void force()
+	{
+		System.out.println("To transmit accelerating and braking force to the ground");
+	}
+	static void direction()
+	{
+		System.out.println ("To change/maintain direction");
+	}
+	static void friction ()
+	{
+		System.out.println("To absorb shock from the road surface");
+	}
+	static void grip()
+	{
+		System.out.println("Used to vehicles grip");
+		friction();
+	}
+}
